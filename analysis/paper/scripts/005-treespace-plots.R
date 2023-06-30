0260cc6628250961f3cb4707ef99405238ac026e
Ben Marwick <benmarwick@hotmail.com>
2023-06-29 20:25:31 -0700

    densitree, treespace and map tree plot

--- analysis/paper/scripts/005-treespace-plots.R
+++ analysis/paper/scripts/005-treespace-plots.R
@@ -8,9 +8,9 @@ library(cowplot)
 outsumfile <- ape::read.nexus(here::here("analysis/data/derived_data/output-01/map_tree.nex"))
 
 # read in the tree space
-phy <- ape::read.tree(here::here("analysis/data/derived_data/output-01/tree_trace.trees" ) )
-phy <- di2multi(phy, tol = 1e-08) # remove -ve branch lengths
-phy <- phy[1:500]
+phy_raw <- ape::read.tree(here::here("analysis/data/derived_data/output-01/tree_trace.trees" ) )
+phy_raw <- di2multi(phy_raw, tol = 1e-08) # remove -ve branch lengths
+phy <- phy_raw[1:1000]
 
 # use treespace
 res <- treespace::treespace(phy, nf=3)
@@ -38,18 +38,16 @@ plotGroves_plot <- grid::grid.grab(wrap.grobs = TRUE)
 
 # https://thibautjombart.github.io/treespace/articles/introduction.html
 
+options(ignore.negative.edge=FALSE)
 outsumfile_tree_plot <-
 ggtree(outsumfile) %<+% tip_labels +  # names_artefacts_ID_and_period_unite +
   theme_tree() +
-  geom_tiplab(size=2,
+  geom_tiplab(size=1.5,
               aes(label = ID_country,
                   color = Period)) +
-  geom_treescale()  +
-  coord_cartesian(clip = 'off') +
-  theme_tree2(plot.margin=margin(6, 120, 6, 6)) +
   theme(legend.position = c(0.2, 0.95),
        legend.spacing.y = unit(0.1, 'mm'),
-       legend.key.size = unit(1, "mm"),
+       legend.key.size = unit(1.5, "mm"),
        legend.title=element_text(size = 6),
        legend.text=element_text(size = 4))
 
@@ -59,26 +57,39 @@ ggdensitree(phy,
             alpha= .01,
             colour='steelblue') %<+% tip_labels +
   theme_tree() +
-  geom_tiplab(size=2,
+  geom_tiplab(size=1.5,
               aes(label = ID_country,
                   color = Period)) +
-  geom_treescale() +
   guides(label = "none",
          color = "none")
 
 #-------------------------------------------------
-# single panel
+# compose a single panel
 library(cowplot)
-plot_grid(plotGroves_plot,
-          outsumfile_tree_plot,
-          densitree_plot,
-          align = "v",
-          axis = "tb",
-          rel_widths = c(3, 3, 5),
-          ncol = 3)
 
-ggsave(here::here("analysis/figures/treespace-map-densitree.png"),
-      w = 7,
+plot_grid(plotGroves_plot)
+
+ggsave(here::here("analysis/figures/treespace-map.png"),
+       w = 5,
+       h = 5)
+
+plot_grid(densitree_plot,
+          outsumfile_tree_plot
+            )
+
+ggsave(here::here("analysis/figures/map-tree-densitree.png"),
+      w = 6,
       h = 5)
 
+# combine the png files of the images
+# we are using imagemagik, not an R pkg
+# brew install imagemagick
+
+system(paste0("convert +append ",
+              here("analysis/figures/treespace-map.png"),
+              " ",
+              here("analysis/figures/map-tree-densitree.png"),
+              " ",
+              "-resize x1000 ",
+              here("analysis/figures/treespace-map-densitree.png")))
 
